7a11d197c740957abc9c7782d95a58829b25a89f
Constantine Molchanov <moigagoo@live.com>
2020-11-19 14:15:27 +0400

    Add index route.

--- src/app.nim
+++ src/app.nim
@@ -5,8 +5,27 @@ import dotenv
 import app/routes/api
 
 
+const indexHtml = """
+<!DOCTYPE html>
+<html>
+<head>
+  <meta content="width=device-width, initial-scale=1" name="viewport" />
+  <title>index</title>
+
+</head>
+<body id="body">
+<div id="ROOT" />
+<script type="text/javascript" src="app.js"></script>
+</body>
+</html>
+"""
+
+
 initDotEnv().load()
 
 
 routes:
+  get "/":
+    resp indexHtml
+
   extend api, "/api"
