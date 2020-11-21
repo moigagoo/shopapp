fe77c46fe280be1734030a5b241a7b396731453e
Constantine Molchanov <moigagoo@live.com>
2020-11-21 22:44:26 +0400

    Rename index.nim to app.nim.

---
+++ src/app/frontend/app.nim
@@ -0,0 +1,35 @@
+import strutils
+
+include karax/prelude
+
+import pages/items
+
+
+type
+  State = ref object
+    itemsPage: ItemsPage
+
+
+proc newState: State =
+  State(itemsPage: newItemsPage())
+
+
+proc run =
+  var state = newState()
+
+  proc render(ctx: RouterData): VNode =
+    buildHtml(tdiv):
+      nav:
+        ul:
+          li: a(href = ""): text "🏠"
+          li: a(href = "#items"): text "Items"
+
+      main:
+        if ctx.hashPart.startsWith("#items"):
+          state.itemsPage.render(ctx)
+
+  setRenderer render
+
+
+when isMainModule:
+  run()
