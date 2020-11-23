74d76755454970c9dbaa87b26c856acfcbe1064e
Constantine Molchanov <moigagoo@live.com>
2020-11-24 00:13:08 +0400

    Add loader.

---
+++ src/app/frontend/components/loader.nim
@@ -0,0 +1,14 @@
+include karax/prelude
+import karax/vstyles
+
+
+proc renderLoader*: VNode =
+  buildHtml:
+    tdiv(style = {
+      border: "16px solid #f3f3f3",
+      borderRadius: "50%",
+      borderTop: "16px solid #3498db",
+      width: "120px",
+      height: "120px",
+      animation: "spin 2s linear infinite"
+    })
