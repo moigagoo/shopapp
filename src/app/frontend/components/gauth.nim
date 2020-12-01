06d9b54f7052765eb03d6a57644e8646176262fb
Constantine Molchanov <moigagoo@live.com>
2020-12-01 23:36:16 +0400

    Remove signout.

--- src/app/frontend/components/gauth.nim
+++ src/app/frontend/components/gauth.nim
@@ -9,18 +9,6 @@ const clientId: kstring = "57979449313-sakjh4lfhc85q0otlo9ic7b0b0iv7cse.apps.goo
 var gapi {.importc.}: JsObject
 
 
-type
-  GAuth* = ref object
-    loaded*, signedIn*: bool
-    user*: JsObject
-
-
-func newGAuth*(loaded, signedIn: bool, user: JsObject): GAuth =
-  GAuth(loaded: loaded, signedIn: signedIn, user: user)
-
-func newGAuth*: GAuth =
-  newGAuth(false, false, nil)
-
 proc init {.exportc.} =
   gapi.load("auth2")
 
@@ -36,7 +24,7 @@ proc doSignIn(clientCfg: JsObject): Future[JsObject] {.async.} =
     resolve user.signIn()
   )
 
-proc signIn(state: var GAuth, clientCfg: JsObject) {.async.} =
+proc signIn(clientCfg: JsObject) {.async.} =
   let
     user = await doSignIn(clientCfg)
     profile = user.getBasicProfile()
@@ -45,36 +33,14 @@ proc signIn(state: var GAuth, clientCfg: JsObject) {.async.} =
   console.log(profile.getGivenName())
   console.log(authResp.id_token)
 
-  state.user = user
-  state.signedIn = true
-
-proc doSignOut(user: JsObject): Future[JsObject] {.async.} =
-  newPromise(proc(resolve: proc(resp: JsObject)) =
-    resolve user.signOut()
-  )
-
-proc signOut(state: var GAuth) {.async.} =
-  discard await doSignOut(state.user)
-
-  state.user = nil
-  state.signedIn = false
-
-proc render*(state: var GAuth, ctx: RouterData): VNode =
-  if not state.loaded:
-    loadScript("https://apis.google.com/js/platform.js?onload=init")
-    state.loaded = true
+proc renderGoogleButton*: VNode =
+  loadScript("https://apis.google.com/js/platform.js?onload=init")
 
   buildHtml:
     button:
-      if not state.signedIn:
-        text "Sign in with Google"
-        proc onClick =
-          discard state.signIn(JsObject{client_id: clientId})
-
-      else:
-        text "Sign out"
-        proc onClick =
-          discard state.signOut()
+      text "Sign in with Google"
+      proc onClick =
+        discard signIn(JsObject{client_id: clientId})
 
         # gapi.auth2.init(clientCfg).then(
         #   proc(user: JsObject) = user.signIn().then(
