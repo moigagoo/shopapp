bb948705ec472c5dd6800cc29fc4b5f9ffb718c4
Constantine Molchanov <moigagoo@live.com>
2020-12-02 15:48:05 +0400

    Add sign out.

--- src/app/frontend/components/gauth.nim
+++ src/app/frontend/components/gauth.nim
@@ -9,47 +9,67 @@ const clientId: kstring = "57979449313-sakjh4lfhc85q0otlo9ic7b0b0iv7cse.apps.goo
 var gapi {.importc.}: JsObject
 
 
+type
+  GoogleButton* = ref object
+    signedIn*: bool
+
+func newGoogleButton*(signedIn: bool): GoogleButton =
+  GoogleButton(signedIn: signedIn)
+
+func newGoogleButton*: GoogleButton =
+  newGoogleButton(false)
+
+
 proc init {.exportc.} =
   gapi.load("auth2")
 
-proc getUser(clientCfg: JsObject): Future[JsObject] {.async.} =
-  newPromise(proc(resolve: proc(resp: JsObject)) =
-    resolve gapi.auth2.init(clientCfg)
-  )
+# proc getUser(clientCfg: JsObject): Future[JsObject] {.async.} =
+#   newPromise(proc(resolve: proc(resp: JsObject)) =
+#     resolve gapi.auth2.init(clientCfg)
+#   )
 
-proc doSignIn(clientCfg: JsObject): Future[JsObject] {.async.} =
-  var user = await getUser(clientCfg)
+# proc doSignIn(clientCfg: JsObject): Future[JsObject] {.async.} =
+#   var user = await getUser(clientCfg)
 
-  newPromise(proc(resolve: proc(resp: JsObject)) =
-    resolve user.signIn()
-  )
+#   newPromise(proc(resolve: proc(resp: JsObject)) =
+#     resolve user.signIn()
+#   )
 
-proc signIn(clientCfg: JsObject) {.async.} =
-  let
-    user = await doSignIn(clientCfg)
-    profile = user.getBasicProfile()
-    authResp = user.getAuthResponse()
+# proc signIn(clientCfg: JsObject) {.async.} =
+#   let
+#     user = await doSignIn(clientCfg)
+#     profile = user.getBasicProfile()
+#     authResp = user.getAuthResponse()
 
-  console.log(profile.getGivenName())
-  console.log(authResp.id_token)
+#   console.log(profile.getGivenName())
+#   console.log(authResp.id_token)
 
-proc renderGoogleButton*: VNode =
+proc render*(state: var GoogleButton, ctx: RouterData): VNode =
   loadScript("https://apis.google.com/js/platform.js?onload=init")
 
   buildHtml:
     button:
-      text "Sign in with Google"
-      proc onClick =
-        discard signIn(JsObject{client_id: clientId})
+      if not state.signedIn:
+        text "Sign in with Google"
+        proc onClick =
+          gapi.auth2.init(JsObject{client_id: clientId}).then(
+            proc(user: JsObject) = user.signIn().then(
+              proc(user: JsObject) =
+                let
+                  profile = user.getBasicProfile()
+                  authResp = user.getAuthResponse()
 
-        # gapi.auth2.init(clientCfg).then(
-        #   proc(user: JsObject) = user.signIn().then(
-        #     proc(user: JsObject) =
-        #       let
-        #         profile = user.getBasicProfile()
-        #         authResp = user.getAuthResponse()
+                console.log(profile.getGivenName())
+                console.log(authResp.id_token)
 
-        #       console.log(profile.getGivenName())
-        #       console.log(authResp.id_token)
-        #   )
-        # )
+                state.signedIn = true
+                redraw()
+            )
+          )
+        # discard signIn(JsObject{client_id: clientId})
+
+      else:
+        text "Sign out"
+        proc onClick =
+          let auth2 = gapi.auth2.getAuthInstance()
+          auth2.signOut().then(proc = state.signedIn = false)
